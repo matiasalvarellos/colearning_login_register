fb672a14dadc60093a16df552a9f20c31782c003
matias <matias.alvarellos@gmail.com>
2022-03-21 17:53:53 -0300

    resolucion

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,14 @@
 const fs = require("fs");
+const bcrypt = require("bcryptjs");
 const path = require("path");
+const { validationResult } = require("express-validator");
+
 const users = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/users.json")))
+
+function writeFile(array){
+    const usersString = JSON.stringify(array, null, 4);
+    fs.writeFileSync(path.join(__dirname, "../data/users.json"), usersString);
+}
 
 module.exports = {
     login: function(req, res){
@@ -8,5 +16,43 @@ module.exports = {
     },
     register: function(req, res){
         res.render("register");
+    },
+    processRegister: function(req, res){
+        
+        const errors = validationResult(req);
+
+        if(errors.errors.length > 1){
+           return res.render("register", {errors: errors.array(), old: req.body}) 
+        }
+        let newUser = {
+            email: req.body.email,
+            name: req.body.name,
+            last_name: req.body.last_name,
+            password: bcrypt.hashSync(req.body.password, 10)
+        }
+        users.push(newUser);
+
+        writeFile(users)
+
+        res.redirect("/");            
+        
+    },
+    processLogin: function(req, res){
+        const errors = validationResult(req);
+        if(errors.errors.length > 1){
+
+        }
+        let user = users.find(user=>{
+            return user.email == req.body.email;
+        })
+        delete user.password;
+        
+        req.session.userLogged = user;
+
+        if(req.body.recordar){
+            res.cookie("recordar", user, {maxAge: 60000 * 24})
+        }
+        res.redirect("/")
+        
     }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,51 @@
+const fs = require("fs");
+const path = require("path");
+const bcrypt = require("bcryptjs");
+const { body } = require("express-validator")
 const express = require('express');
 const router = express.Router();
 const userController = require("../controllers/userController");
-
+const users = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/users.json")))
 /* GET users listing. */
 
 //register
 router.get("/register", userController.register);
+router.post("/register", [ 
+  body("name")
+    .notEmpty()
+    .withMessage("campo nombre vacio"),
+  body("email")
+    .notEmpty()
+    .withMessage("campo email vacio")
+    .bail()
+    .isEmail()
+    .withMessage("formato desconocido"),
+],
+ userController.processRegister
+);
 
 //login
 router.get('/login', userController.login);
+router.post("/login",[ 
+  body("email")
+    .notEmpty()
+    .withMessage("campo email vacio")
+    .bail()
+    .isEmail()
+    .withMessage("formato desconocido"),
+  body("password")
+    .notEmpty()
+    .withMessage("campo password vacio")
+    .custom(function(value, {req}){
+      let usuarioFound = users.find(function(user){
+        return user.email == req.body.email && bcrypt.compareSync(value, usuarioFound.password) ;
+      })
+      if(!usuarioFound){
+        throw new Error("Error en las credenciales")
+      }
+    })
+] 
+,userController.processLogin);
 
 
 module.exports = router;
