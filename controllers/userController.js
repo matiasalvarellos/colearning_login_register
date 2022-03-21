79ddea3949589f84199f7ff64bc44b1a1dfa0192
matias <matias.alvarellos@gmail.com>
2022-03-21 15:21:03 -0300

    add data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+const users = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/users.json")))
+
 module.exports = {
     login: function(req, res){
-        res.render("login")
-
+        res.render("login");
     },
     register: function(req, res){
-
-        res.render("register")
+        res.render("register");
     }
 }
